185b77046ec816add9b7653c3e401af3b770cd56
m-vanhoose <m-vanhoose@live.com>
2023-04-05 11:50:04 -0400

    fixed issues with api added day0 info

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ document.getElementById('search').addEventListener('click', function(event){
 })
 
 let citySearch = function(city) {
-    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3ef2a12f03e1ed36d30c2f2797577759';
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=3ef2a12f03e1ed36d30c2f2797577759&units=imperial';
     fetch(requestUrl)
         .then(function (response) {
         return response.json();
@@ -16,6 +16,8 @@ let citySearch = function(city) {
     
     let cards = document.getElementById('cards');
     cards.classList.remove('hide');
+    let ul = document.getElementById('ul');
+    ul.classList.remove('hide');
     let weatherData = document.getElementById('weatherdata');
     weatherData.innerHTML = ''
     let nameEl = document.createElement('p');
@@ -30,7 +32,12 @@ let citySearch = function(city) {
     imgEl.src = 'http://openweathermap.org/img/wn/'+data.weather[0].icon+'@2x.png';
     weatherData.appendChild(imgEl);
     imgEl.className = 'img';
-
+    let temp0 = document.getElementById('temp0');
+    temp0.innerText = data.main.temp +' °F';
+    let wind0 = document.getElementById('wind0');
+    wind0.innerText = data.wind.speed + ' MPH';
+    let hum0 = document.getElementById('hum0');
+    hum0.innerText = data.main.humidity + ' %';
     });
 
     let requestUrl2 = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city +'&units=imperial&appid=3ef2a12f03e1ed36d30c2f2797577759';
@@ -42,19 +49,19 @@ let citySearch = function(city) {
             console.log(data);
         
             let day1 = document.getElementById('day1');
-            day1.innerText = dayjs.unix(data.list[1].dt).format('MMMM D YYYY');
+            day1.innerText = dayjs.unix(data.list[10].dt).format('MMMM D YYYY');
             let imgEl1 = document.createElement('img');
-            imgEl1.src = 'http://openweathermap.org/img/wn/'+data.list[1].weather[0].icon+'@2x.png';
+            imgEl1.src = 'http://openweathermap.org/img/wn/'+data.list[2].weather[0].icon+'@2x.png';
             day1.appendChild(imgEl1)
             let temp1 = document.getElementById('temp1');
-            temp1.innerText = data.list[1].main.temp +' °F';
+            temp1.innerText = data.list[2].main.temp +' °F';
             let wind1 = document.getElementById('wind1');
-            wind1.innerText = data.list[1].wind.speed +' MPH';
+            wind1.innerText = data.list[2].wind.speed +' MPH';
             let hum1 = document.getElementById('hum1');
-            hum1.innerText = data.list[1].main.humidity +' %';
+            hum1.innerText = data.list[2].main.humidity +' %';
 
             let day2 = document.getElementById('day2');
-            day2.innerText = dayjs.unix(data.list[9].dt).format('MMMM D YYYY');
+            day2.innerText = dayjs.unix(data.list[17].dt).format('MMMM D YYYY');
             let imgEl2 = document.createElement('img');
             imgEl2.src = 'http://openweathermap.org/img/wn/'+data.list[9].weather[0].icon+'@2x.png';
             day2.appendChild(imgEl2)
@@ -66,19 +73,19 @@ let citySearch = function(city) {
             hum2.innerText = data.list[9].main.humidity +' %';
 
             let day3 = document.getElementById('day3');
-            day3.innerText = dayjs.unix(data.list[17].dt).format('MMMM D YYYY');
+            day3.innerText = dayjs.unix(data.list[25].dt).format('MMMM D YYYY');
             let imgEl3 = document.createElement('img');
             imgEl3.src = 'http://openweathermap.org/img/wn/'+data.list[17].weather[0].icon+'@2x.png';
             day3.appendChild(imgEl3)
             let temp3 = document.getElementById('temp3');
-            // temp3.innerText = data.list[17].main.temp +' °F'
+            temp3.innerText = data.list[17].main.temp +' °F';
             let wind3 = document.getElementById('wind3');
             wind3.innerText = data.list[17].wind.speed +' MPH';
             let hum3 = document.getElementById('hum3');
             hum3.innerText = data.list[17].main.humidity +' %';
 
             let day4 = document.getElementById('day4');
-            day4.innerText = dayjs.unix(data.list[25].dt).format('MMMM D YYYY');
+            day4.innerText = dayjs.unix(data.list[33].dt).format('MMMM D YYYY');
             let imgEl4 = document.createElement('img');
             imgEl4.src = 'http://openweathermap.org/img/wn/'+data.list[25].weather[0].icon+'@2x.png';
             day4.appendChild(imgEl4)
@@ -90,7 +97,7 @@ let citySearch = function(city) {
             hum4.innerText = data.list[25].main.humidity +' %';
 
             let day5 = document.getElementById('day5');
-            day5.innerText = dayjs.unix(data.list[33].dt).format('MMMM D YYYY');
+            day5.innerText = dayjs.unix(data.list[39].dt).format('MMMM D YYYY');
             let imgEl5 = document.createElement('img');
             imgEl5.src = 'http://openweathermap.org/img/wn/'+data.list[33].weather[0].icon+'@2x.png';
             day5.appendChild(imgEl5)
